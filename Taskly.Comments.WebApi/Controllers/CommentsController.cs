e85f8afeafd90b98583bd2e22c29be4601bc2fb2
Mikhail Doshevsky <m.doshevsky@gmail.com>
2020-05-05 18:47:38 +0300

    Extends API

--- Taskly.Comments.WebApi/Controllers/CommentsController.cs
+++ Taskly.Comments.WebApi/Controllers/CommentsController.cs
@@ -9,10 +9,10 @@ namespace Taskly.Comments.WebApi.Controllers
     [Route("api/comments")]
     public class CommentsController : ControllerBase
     {
-        [HttpGet("{locator}")]
+        [HttpGet("list/{locator}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult<CommentsListDto> GetComments(string locator)
+        public ActionResult<CommentsListDto> GetCommentsList(string locator)
         {
             var dto = new CommentsListDto
             {
@@ -35,12 +35,47 @@ namespace Taskly.Comments.WebApi.Controllers
             return Ok(dto);
         }
 
-        [HttpPost]
+        [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public ActionResult CreateComment([FromBody] CommentCreateDto dto)
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<CommentDto> GetComment(string id)
         {
-            return Ok();
+            var dto = new CommentDto
+            {
+                Id = id,
+                ParentId = 0.ToString(),
+                AuthorId = 350.ToString(),
+                Text = $"Some comment text.",
+                Timestamp = DateTime.UtcNow
+            };
+
+            return Ok(dto);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<CommentDto> CreateComment([FromBody] CommentCreateDto dto)
+        {
+            var commentDto = new CommentDto
+            {
+                Id = 367.ToString(),
+                ParentId = dto.ParentId,
+                AuthorId = dto.AuthorId,
+                Text = dto.Text,
+                Timestamp = DateTime.UtcNow
+            };
+
+            return CreatedAtAction(nameof(GetComment), new { id = commentDto.Id }, commentDto);
+        }
+
+        [HttpDelete("id")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult DeleteComment(string id)
+        {
+            return NoContent();
         }
     }
 }
